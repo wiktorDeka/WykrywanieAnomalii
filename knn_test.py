14f1efb0366acd996db268fcb84bcbfa3298ba71
Kliprmimo <grzes.dziechciarz@gmail.com>
2024-05-14 21:40:57 +0200

    added option to choose target encoding

--- knn_test.py
+++ knn_test.py
@@ -1,37 +1,45 @@
 from sklearn import neighbors
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import TargetEncoder
 import pandas as pd
 import numpy as np
 # network_data = pd.read_csv('reduced_dataset_sdn.csv')
 
 network_data = pd.read_csv('dataset_sdn.csv')
+ip_encoding = 1
 
-src_label_encoder = LabelEncoder()
-network_data['src_ip_encoded'] = src_label_encoder.fit_transform(network_data['src'])
-network_data.drop('src', axis=1, inplace=True)
+if ip_encoding == 0:
+    label_encoder = LabelEncoder()
+    network_data['src_ip_encoded'] = label_encoder.fit_transform(network_data['src'])
+    network_data.drop('src', axis=1, inplace=True)
 
-dst_label_encoder = LabelEncoder()
-network_data['dst_ip_encoded'] = dst_label_encoder.fit_transform(network_data['dst'])
-network_data.drop('dst', axis=1, inplace=True)
+    network_data['dst_ip_encoded'] = label_encoder.fit_transform(network_data['dst'])
+    network_data.drop('dst', axis=1, inplace=True)
+
+    network_data['protocol_encoded'] = label_encoder.fit_transform(network_data['Protocol'])
+    network_data.drop('Protocol', axis=1, inplace=True)
+
+    network_data.dropna(inplace=True)
+
+    y = network_data['label']
+    X = network_data.drop('label', axis=1)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
 
-network_data['protocol_encoded'] = dst_label_encoder.fit_transform(network_data['Protocol'])
-network_data.drop('Protocol', axis=1, inplace=True)
+elif ip_encoding == 1:
+    te = TargetEncoder(smooth='auto', target_type='binary')
+    y = network_data['label']
+    X = network_data.drop('label', axis=1)
+    target = network_data['src']
+
+    X_trans = te.fit_transform(X, target)
+    X_train, X_test, y_train, y_test = train_test_split(X_trans, y, test_size=0.3, random_state=0)
 
 # replace or delete all NaN values from dataset
 # print(np.count_nonzero(network_data.isnull().values))
 print(network_data.isna().sum())
-
-network_data.dropna(inplace=True)
-# network_data.fillna(value=0, inplace=True)
-
-# print(np.count_nonzero(network_data.isnull().values))
-print(network_data.isna().sum())
-
-y = network_data['label']
-X = network_data.drop('label', axis=1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
 K = []
 training = []
