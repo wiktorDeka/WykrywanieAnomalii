938062c8dc380daba6ced7d488c5c10c16e33199
Kliprmimo <grzes.dziechciarz@gmail.com>
2024-05-27 13:57:02 +0200

    added one class svm and testing with different gammas and nu

--- svm.py
+++ svm.py
@@ -15,9 +15,23 @@ if __name__ == '__main__':
     # target_encoding is great (1)
     ip_encoding = 1
     X_train, X_test, y_train, y_test = encode_data(network_data, ip_encoding)
-    ksvm = svm.SVC(kernel='rbf', gamma=0.1)
-    # Train the model on the training data
-    ksvm.fit(X_train, y_train)
-    # Evaluate the model on the test data
-    accuracy = ksvm.score(X_test, y_test)
-    print('Accuracy:', accuracy)
+    for i in range(1, 20, 1):
+        actual_gamma = i / 100
+        ksvm = svm.SVC(kernel='rbf', gamma=actual_gamma)
+        # Train the model on the training data
+        ksvm.fit(X_train, y_train)
+        # Evaluate the model on the test data
+        accuracy = ksvm.score(X_test, y_test)
+        print(f'Accuracy with gamma {actual_gamma} :', accuracy)
+    for j in range(1, 20, 1):
+        for i in range(1, 20, 1):
+            actual_gamma = i / 100
+            actual_nu = j / 100
+            clf = svm.OneClassSVM(kernel='rbf', gamma=actual_gamma, nu=actual_nu)
+            clf.fit(X_train)
+
+            y_pred_test = clf.predict(X_test)
+            y_pred_test = [0 if y == 1 else 1 for y in y_pred_test]
+            n_error_test = (y_pred_test != y_test).sum()
+            test_accuracy = (len(y_test) - n_error_test) / len(y_test)
+            print(f'One-Class SVM accuracy with gamma {actual_gamma} and nu {actual_nu}:', test_accuracy)
